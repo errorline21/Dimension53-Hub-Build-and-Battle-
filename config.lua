c94047f5f48890d08010def8664533336da89ff4
lostremors3 <errorline21@gmail.com>
2026-08-22 16:55:00 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
     PublicUsage = true,
 
-    -- Increment this if you want to require a newer loader later.
+    
     Version = 1,
 
     DisabledMessage = "Dimension53Hub is currently disabled."
