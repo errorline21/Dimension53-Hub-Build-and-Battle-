4dfabf09b8bb0c34987df34486e03cd86cb264b4
lostremors3 <errorline21@gmail.com>
2026-09-20 21:36:14 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 return {
-    PublicUsage = true,
+    PublicUsage = false,
 
     
     Version = 8,
 
-    DisabledMessage = "Azimuth_Pi Hub is currently disabled."
+    DisabledMessage = "Azimuth_Pi Hub is currently disabled. Since uploading this hub, the game population has decreased 16x. I enjoy this simplistic and nostalgic game and don't want it ruined."
 }
